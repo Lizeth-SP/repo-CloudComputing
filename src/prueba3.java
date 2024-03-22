6fdce4d34117f4d42d418507ffd44df47ca8a40f
Lizeth <3BSlizethSP@gmail.com>
2024-03-22 17:10:29 -0600

    Primer commit

---
+++ src/prueba3.java
@@ -0,0 +1,19 @@
+
+public class prueba3 {
+    public static void main(String[] args) {
+        int[] calificaciones = {70, 80, 90, 88, 100};
+        int sum = 0;
+
+        for (int calificacion : calificaciones) {
+            sum += calificacion;
+        }
+
+        double promedio = (double) sum / calificaciones.length;
+
+        if (promedio >= 60) {
+            System.out.println("El estudiante ha aprobado con un promedio de " + promedio);
+        } else {
+            System.out.println("El estudiante NO ha aprobado con un promedio de " + promedio);
+        }
+    }
+}
